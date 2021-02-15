d245d0775f2f02014d7f15d682a94587e724e311
Jay Moliya <52040275+JayMoliya33@users.noreply.github.com>
2021-02-15 09:58:57 +0530

    fix small issue in mapview

--- app/src/main/java/com/example/googlemapsappdemo/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsappdemo/MapsActivity.kt
@@ -3,6 +3,7 @@ package com.example.googlemapsappdemo
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
@@ -23,9 +24,11 @@ import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 import java.io.IOException
-import java.lang.IndexOutOfBoundsException
 import java.util.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraIdleListener, GoogleMap.OnCameraMoveStartedListener {
 
@@ -72,8 +75,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
 
     private fun askForUserPermission() {
         askPermission(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
         ) {
 
             getCurrentLocation()
@@ -108,12 +111,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
 
         // Check Permission
         if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
         ) {
             return
         }
@@ -135,10 +138,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
                 if (loc.isSuccessful) {
                     val currentLocation = loc.result
                     if (currentLocation != null) {
-                        moveCamera(
-                            LatLng(currentLocation.latitude, currentLocation.longitude),
-                            DEFAULT_ZOOM
-                        )
+                        moveCamera(LatLng(currentLocation.latitude, currentLocation.longitude), DEFAULT_ZOOM)
                     }
                 } else {
                     askForUserPermission()
@@ -157,19 +157,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         val geocoder = Geocoder(this, Locale.getDefault())
         var addresses : List<Address>? = null
         try {
-            addresses = geocoder.getFromLocation(location.latitude,location.longitude,1)
+            addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
         }catch (e: Exception){
             e.printStackTrace()
         }
         setAddress(addresses!![0])
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setAddress(address: Address){
-            if (address!== null){
-                val latlang = LatLng(mMap!!.cameraPosition.target.latitude,mMap!!.cameraPosition.target.longitude)
+            if (address!= null){
+                val latlang = LatLng(mMap!!.cameraPosition.target.latitude, mMap!!.cameraPosition.target.longitude)
                 if (address.getAddressLine(0)!=null){
                     tvCurrentAddress.text = address.getAddressLine(0)
-                    mMap?.addMarker(MarkerOptions().position(latlang).title(tvCurrentAddress.text.toString()))?.showInfoWindow()
+                   // mMap?.addMarker(MarkerOptions().position(latlang).title(tvCurrentAddress.text.toString()))?.showInfoWindow()
 
                 }
                 if (address.getAddressLine(1)!=null){
@@ -188,8 +189,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, 
         val geocoder = Geocoder(this, Locale.getDefault())
         var addresses : List<Address>? = null
         try {
-            addresses = geocoder.getFromLocation(mMap!!.cameraPosition.target.latitude,mMap!!.cameraPosition.target.longitude,1)
+            addresses = geocoder.getFromLocation(mMap!!.cameraPosition.target.latitude, mMap!!.cameraPosition.target.longitude, 1)
             setAddress(addresses!![0])
+            mMap?.setOnMapClickListener {
+                // set market on onclick
+                mMap?.addMarker(MarkerOptions().position(it).title(tvCurrentAddress.text.toString()))?.showInfoWindow()
+
+                // mMap!!.clear()
+                //  mMap!!.addMarker(MarkerOptions().position(it))
+            }
         }catch (e: IndexOutOfBoundsException){
             e.printStackTrace()
         }catch (e: IOException){
